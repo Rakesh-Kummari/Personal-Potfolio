8bc39bb74af417e47a0b861046c7f48ed6d47375
Rakesh Kummari <rajbabukummari0341@gmail.com>
2025-07-15 16:11:41 +0530

    check are cheked

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -22,8 +22,9 @@ function Services() {
           </div>
           <div className="services-body">
             <p style={{ color: theme.tertiary80 }}>
-              These are some of the services I offer. Reach out to me if I can
-              help you with any!
+              I'm passionate about Java and MERN stack development, with a
+              creative edge in photography and video editing. I also enjoy
+              reading books, which keeps me constantly learning and inspired!
             </p>
             <div className="services-bodycontainer">
               {servicesData.map((services) => (
